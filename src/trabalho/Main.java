93384eb12bfc2de491f69d2f88e23e6a9b47ba8c
Cassiano Flores <cassiano.flores@edu.pucrs.br>
2021-07-05 20:24:44 -0300

    minha parte

--- src/trabalho/Main.java
+++ src/trabalho/Main.java
@@ -1,8 +1,11 @@
 package trabalho;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
 	// write your code here
     }
 }
